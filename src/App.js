5c376e7b0ef0d88028514dfe13f113b3081dc961
Your Name <you@example.com>
2024-01-21 00:45:00 +0200

    add share button

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
-import './App.css';
-import React, {useState} from 'react';
+import "./App.css";
+import { useState } from "react";
 
+import { RWebShare } from "react-web-share";
+import { FaShareAlt } from "react-icons/fa";
 const App = () => {
   const url = "https://api.quotable.io/random";
   let quoteData = {
     content: "Let time be your only competitor.",
-    author: "Ahmed Saber"
-  }
-  const [quote, setQuote] = useState(quoteData)
+    author: "Ahmed Saber",
+  };
+  const [quote, setQuote] = useState(quoteData);
+  // const location = useLocation();
 
   const generateQuote = () => {
     fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data)
-        setQuote(data)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setQuote(data);
       });
-  }
+  };
 
   const copy = () => {
-    navigator.clipboard.writeText(quote.author + " once said: " + quote.content)
-    alert('copied')
-  }
-
+    navigator.clipboard.writeText(
+      quote.author + " once said: " + quote.content
+    );
+    alert("copied");
+  };
   return (
     <>
       <h1>Quote Generator React App</h1>
@@ -30,13 +34,24 @@ const App = () => {
         <p>{quote.content}</p>
         <span>{quote.author}</span>
         <div className="btns">
-          <button onClick={copy} className="btn">Copy</button>
+          <button onClick={copy} className="btn">
+            Copy
+          </button>
+
+          <button className="btn">
+            <RWebShare
+              data={{
+                title: quote.author + " once said: " + quote.content,
+              }}
+              onClick={() => {}}
+              children={<FaShareAlt />}
+            ></RWebShare>
+          </button>
           <button onClick={generateQuote}>Generate Another Quote</button>
         </div>
       </div>
     </>
-  )
-}
-
+  );
+};
 
 export default App;
